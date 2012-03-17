382860746027ae5dd80a1f8e575f6b4a292bf408
Stephen Haywood <stephen@averagesecurityguy.info>
2012-03-16 21:41:21 -0400

    Removed failed attempt at datastore.

--- prometheus.rb
+++ prometheus.rb
@@ -11,33 +11,29 @@ end
 $:.unshift(File.join(File.dirname(base), 'lib'))
 
 require 'optparse'
-require 'datastore'
 
-# Configure the Datastore
-ds = Prometheus::DataStore.new
-ds.options[:base] = base
-
+options = {}
 optparse = OptionParser.new do|opts|
 	# Firewall configuration file
-	ds.options[:config] = ""
+	options[:config] = ""
 	opts.on( '-c', '--config_file FILE', "Firewall configuration to parse." ) do|c|
 		options[:config] = c
 	end
 
 	# Report output file
-	ds.options[:report] = "#{Time.now.to_f.to_s}.html"
+	options[:report] = "#{Time.now.to_f.to_s}.html"
 	opts.on( '-r', '--report_file FILE', "Report file to write." ) do |r|
 		options[:report] = r
 	end
 	
 	# Report format
-	ds.options[:format] = "html"
+	options[:format] = "html"
 	opts.on( '-f', '--report_format FORMAT', "Report format to use.") do |f|
 		options[:format] = f
 	end
 
 	# Verbose output
-	ds.options[:verbose] = false
+	options[:verbose] = false
 	opts.on( '-v', '--verbose', "Print verbose output.") do |v|
 		options[:verbose] = true
 	end
@@ -54,32 +50,35 @@ optparse.parse!
 # Begin main program
 require 'errors'
 require 'ui'
-require 'config'
 require 'parse'
 require 'analyze'
 require 'report'
-require 'options'
+
+include PrometheusErrors
+include PrometheusUI
+
+$verbose = options[:verbose]
 
 # Parse the firewall config
 begin
-	ds.firewall = Parse::parse_firewall(ds.options)
-rescue Prometheus::ParseError => e
+	firewall = parse_firewall(options[:config])
+rescue ParseError => e
 	print_error(e.message)
 	exit
 end
 
 # Analyze the firewall config
 begin
-	analysis = Analyze::analyze_firewall(firewall)
-rescue Prometheus::AnalysisError => e
+	analysis = analyze_firewall(firewall)
+rescue AnalysisError => e
 	print_error(e.message)
 	exit
 end
 
 #Create report for firewall config and analysis
 begin
-	Report::report_firewall(firewall, analysis, ds.options)
-rescue Prometheus::ReportError => e
+	report_firewall(firewall, analysis, options[:report], options[:format], options[:template] )
+rescue ReportError => e
 	print_error(e.message)
 	exit
 end
