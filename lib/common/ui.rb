320f5b6ccee3e050ca6b334a8d0955d335e8b180
Stephen Haywood <stephen@averagesecurityguy.info>
2012-05-12 01:28:41 -0400

    Disable color support for Windows

--- lib/common/ui.rb
+++ lib/common/ui.rb
@@ -5,36 +5,22 @@ module PrometheusUI
 
 	##
 	# The UI uses color-coded status messages, which work fine on *nix boxes 
-	# but requires an extra module on Windows. Check to see if we are on 
-	# Windows. If so, try to load the module Win32/Console/ANSI. If it is not 
-	# available ask the user to install the win32console gem. Continue without 
-	# color support.
+	# but requires extra modules on Windows. Check to see if we are on 
+	# Windows. If so, continue without color support.
 	if RUBY_PLATFORM =~ /win32/ or RUBY_PLATFORM =~ /mingw32/
 
-		begin
-			require 'Win32/Console/ANSI'
-		rescue LoadError
-			puts "You must install the win32console gem to use color on "
-			puts "Windows. Proceeding without color support."
-			$color = false
-		end
+		$color = false
  	end
 
 	##
 	# Use ANSI encoding to colorize text.
 	def colorize(text, color_code)
-		if RUBY_PLATFORM =~ /win32/
-			puts "Windows color"
-			"\e[#{color_code}#{text}\e[0m"
-		else
-			puts "Other color"
-			"\033[#{color_code}#{text}\033[0m"
-		end
+		"#{color_code}#{text}\033[0m"
 	end
 
-	def red(text); colorize(text, "31m"); end
-	def green(text); colorize(text, "32m"); end
-	def blue(text); colorize(text, "34m"); end
+	def red(text); colorize(text, "\033[31m"); end
+	def green(text); colorize(text, "\033[32m"); end
+	def blue(text); colorize(text, "\033[34m"); end
 
 	##
 	# Print status messages.
